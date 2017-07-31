483ae0530698e53944f0b737270a8f386d11cfe6
Johan Mabille <johan.mabille@gmail.com>
2017-07-31 11:43:49 +0200

    code cleanup

--- include/xwidgets/xslider.hpp
+++ include/xwidgets/xslider.hpp
@@ -6,43 +6,57 @@
 namespace xeus
 {
     template <class T>
-    class slider final : public xwidget<slider<T>>
+    class xslider final : public xwidget<xslider<T>>
     {
     public:
 
-        using base_type = xwidget<slider<T>>;
+        using base_type = xwidget<xslider<T>>;
 
-        slider() : base_type()
-        {
-            set_defaults();
-            this->open();
-        }
+        xslider();
 
-        XPROPERTY(double, slider, value);
+        XPROPERTY(double, xslider, value);
 
-        inline xjson get_state() const
-        {
-            xjson state = base_type::get_state();
-            state["value"] = value.raw_value();
-            return state;
-        }
-
-        inline void set_state(const xjson& state)
-        {
-            if (state.find("value") != state.end())
-                value = state.at("value").get<double>();
-            //p.address = j.at("address").get<std::string>();
-            //p.age = j.at("age").get<int>();   
-        }
+        xjson get_state() const;
+        void set_state(const xjson& state);
 
     private:
 
-        inline void set_defaults()
-        {
-            base_type::_model_name.raw_value() = "FloatSliderModel";
-            base_type::_view_name.raw_value() = "FloatSliderView";
-        }
+        void set_defaults();
     };
+
+    /**************************
+     * xslider implementation *
+     **************************/
+
+    template <class T>
+    inline xslider<T>::xslider()
+        : base_type()
+    {
+        set_defaults();
+        this->open();
+    }
+
+    template <class T>
+    inline xjson xslider<T>::get_state() const
+    {
+        xjson state = base_type::get_state();
+        state["value"] = value();
+        return state;
+    }
+
+    template <class T>
+    inline void xslider<T>::set_state(const xjson& state)
+    {
+        if (state.find("value") != state.end())
+            value = state.at("value").get<double>();
+    }
+
+    template <class T>
+    inline void xslider<T>::set_defaults()
+    {
+        base_type::_model_name() = "FloatSliderModel";
+        base_type::_view_name() = "FloatSliderView";
+    }
 }
 
 #endif
