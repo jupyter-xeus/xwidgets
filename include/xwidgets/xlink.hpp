245759295278cd0ae550eee9437d645d4942df00
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-11-13 11:10:21 +0100

    Adding directional link

--- include/xwidgets/xlink.hpp
+++ include/xwidgets/xlink.hpp
@@ -72,6 +72,44 @@ namespace xw
 
     using link_generator = xgenerator<xlink>;
 
+    /********************************
+     * directional_link declaration *
+     ********************************/
+
+    using xdirectional_link_pair_type = std::pair<xholder<xtransport>, std::string>;
+
+    template <class D>
+    class xdirectional_link : public xobject<D>
+    {
+    public:
+
+        using base_type = xobject<D>;
+        using derived_type = D;
+
+        using pair_type = xdirectional_link_pair_type;
+
+        xeus::xjson get_state() const;
+        void apply_patch(const xeus::xjson& patch);
+
+        XPROPERTY(pair_type, derived_type, source);
+        XPROPERTY(pair_type, derived_type, target);
+
+    protected:
+
+        template <class S, class T>
+        xdirectional_link(const xtransport<S>& s, std::string sn, xtransport<T>& t, std::string tn);
+
+        using base_type::base_type;
+
+    private:
+
+        void set_defaults();
+    };
+
+    using directional_link = xmaterialize<xdirectional_link>;
+
+    using directional_link_generator = xgenerator<xdirectional_link>;
+
     /************************
      * xlink implementation *
      ************************/
@@ -112,6 +150,49 @@ namespace xw
     inline void xlink<D>::set_defaults()
     {
         this->_model_name() = "LinkModel";
+        this->_model_module() = "@jupyter-widgets/controls";
+    }
+
+    /************************************
+     * xdirectional_link implementation *
+     ************************************/
+
+    template <class D>
+    inline xeus::xjson xdirectional_link<D>::get_state() const
+    {
+        xeus::xjson state = base_type::get_state();
+
+        XOBJECT_SET_PATCH_FROM_PROPERTY(source, state);
+        XOBJECT_SET_PATCH_FROM_PROPERTY(target, state);
+
+        return state;
+    }
+
+    template <class D>
+    inline void xdirectional_link<D>::apply_patch(const xeus::xjson& patch)
+    {
+        base_type::apply_patch(patch);
+
+        XOBJECT_SET_PROPERTY_FROM_PATCH(source, patch)
+        XOBJECT_SET_PROPERTY_FROM_PATCH(target, patch)
+    }
+
+    template <class D>
+    template <class S, class T>
+    inline xdirectional_link<D>::xdirectional_link(const xtransport<S>& s, std::string sn, xtransport<T>& t, std::string tn)
+        : base_type()
+    {
+        set_defaults();
+        this->source().first = s;
+        this->source().second = sn;
+        this->target().first = t;
+        this->target().second = tn;
+    }
+
+    template <class D>
+    inline void xdirectional_link<D>::set_defaults()
+    {
+        this->_model_name() = "DirectionalLinkModel";
         this->_model_module() = "@jupyter-widgets/controls";
     }
 }
