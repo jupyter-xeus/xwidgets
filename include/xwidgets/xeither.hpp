6bebad143629c555852d4ac09da787ac6cfb1104
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-08-12 10:15:45 +0200

    Formatting

--- include/xwidgets/xeither.hpp
+++ include/xwidgets/xeither.hpp
@@ -12,30 +12,37 @@
 #include <stdexcept>
 #include <string>
 #include <unordered_set>
+
 #include "xtl/xoptional.hpp"
 
 template <typename ... T>
-auto XEITHER(T... vals)	{			\
-  const std::unordered_set<std::string> options({vals...});
-  return [options](const auto& proposal) {
-	   auto position = options.find(proposal);
-	   if (position == options.end()) {
+auto XEITHER(T... vals)
+{
+    const std::unordered_set<std::string> options({vals...});
+    return [options](const auto& proposal) {
+        auto position = options.find(proposal);
+        if (position == options.end())
+        {
             throw std::runtime_error("Invalid proposal for string enum");
-	   }					\
-	 };
+        }
+    };
 }
 
 template <typename ... T>
-auto XEITHER_OPTIONAL(T...vals) {
+auto XEITHER_OPTIONAL(T...vals)
+{
     const std::unordered_set<std::string> options({vals...});
-    return [options](const auto& proposal) {
-	     if (xtl::has_value(proposal)) {
-	       auto position = options.find(xtl::value(proposal));
-	       if (position == options.end()) {
-		 throw std::runtime_error("Invalid proposal for optional string enum");
-	       }
-	     }
-	   };
+    return [options](const auto& proposal)
+    {
+        if (xtl::has_value(proposal))
+        {
+            auto position = options.find(xtl::value(proposal));
+            if (position == options.end())
+            {
+                throw std::runtime_error("Invalid proposal for optional string enum");
+            }
+        }
+   };
 }
 
 #endif
