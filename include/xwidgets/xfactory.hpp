11c4ab4abf7a427e5c2cac12dd011f30311ca57e
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-10-25 19:59:54 +0100

    Split xfactory

--- include/xwidgets/xfactory.hpp
+++ include/xwidgets/xfactory.hpp
@@ -12,22 +12,18 @@
 #include <functional>
 #include <string>
 #include <unordered_map>
-#include <utility>
 
 #include "nlohmann/json.hpp"
 
 #include "xeus/xcomm.hpp"
-#include "xeus/xguid.hpp"
+
+#include "xwidgets/xwidgets_config.hpp"
 
 namespace nl = nlohmann;
 
 namespace xw
 {
-    /************************
-     * xfactory declaration *
-     ************************/
-
-    class xfactory
+    XWIDGETS_API class xfactory
     {
     public:
 
@@ -46,35 +42,7 @@ namespace xw
         std::unordered_map<std::string, maker_type> m_makers;
     };
 
-    inline xfactory& get_xfactory();
-
-    /***************************
-     * xfactory implementation *
-     ***************************/
-
-    inline void xfactory::register_maker(const std::string& model_module,
-                                         const std::string& model_name,
-                                         const std::string& view_module,
-                                         const std::string& view_name,
-                                         maker_type maker)
-    {
-        m_makers[model_module + model_name + view_module + view_name] = std::move(maker);
-    }
-
-    inline void xfactory::make(xeus::xcomm&& comm, const nl::json& state, const xeus::buffer_sequence& buffers) const
-    {
-        std::string model_module = state["_model_module"];
-        std::string model_name = state["_model_name"];
-        std::string view_module = state["_view_module"];
-        std::string view_name = state["_view_name"];
-        m_makers.at(model_module + model_name + view_module + view_name)(std::move(comm), state, buffers);
-    }
-
-    inline xfactory& get_xfactory()
-    {
-        static xfactory factory;
-        return factory;
-    }
+    XWIDGETS_API xfactory& get_xfactory();
 }
 
 #endif
---
+++ src/xfactory.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+* Copyright (c) 2017, Sylvain Corlay and Johan Mabille                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "xwidgets/xfactory.hpp"
+
+#include <utility>
+
+namespace nl = nlohmann;
+
+namespace xw
+{
+    void xfactory::register_maker(const std::string& model_module,
+                                  const std::string& model_name,
+                                  const std::string& view_module,
+                                  const std::string& view_name,
+                                  maker_type maker)
+    {
+        m_makers[model_module + model_name + view_module + view_name] = std::move(maker);
+    }
+
+    void xfactory::make(xeus::xcomm&& comm, const nl::json& state, const xeus::buffer_sequence& buffers) const
+    {
+        std::string model_module = state["_model_module"];
+        std::string model_name = state["_model_name"];
+        std::string view_module = state["_view_module"];
+        std::string view_name = state["_view_name"];
+        m_makers.at(model_module + model_name + view_module + view_name)(std::move(comm), state, buffers);
+    }
+
+    xfactory& get_xfactory()
+    {
+        static xfactory factory;
+        return factory;
+    }
+}
+
