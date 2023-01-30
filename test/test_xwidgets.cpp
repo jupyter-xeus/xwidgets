092b5e4b8421132d9fd5e5b855eded14b6d825c1
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-01-30 13:42:36 +0100

    Replace REQUIRE with CHECK in test_widget

--- test/test_xwidgets.cpp
+++ test/test_xwidgets.cpp
@@ -55,8 +55,8 @@ namespace xw
             button_style bs;
             bs.button_color = "red";
             bs.font_weight = "10";
-            REQUIRE_EQ("red", bs.button_color());
-            REQUIRE_EQ("10", bs.font_weight());
+            CHECK_EQ("red", bs.button_color());
+            CHECK_EQ("10", bs.font_weight());
         }
 
         TEST_CASE("button")
@@ -65,127 +65,127 @@ namespace xw
             b.tooltip = "tooltip";
             b.disabled = true;
             b.icon = "icon";
-            REQUIRE_EQ("tooltip", b.tooltip());
-            REQUIRE_EQ(true, b.disabled());
-            REQUIRE_EQ("icon", b.icon());
+            CHECK_EQ("tooltip", b.tooltip());
+            CHECK_EQ(true, b.disabled());
+            CHECK_EQ("icon", b.icon());
         }
 
         TEST_CASE("checkbox")
         {
             checkbox c;
             c.indent = true;
-            REQUIRE_EQ(true, c.indent());
+            CHECK_EQ(true, c.indent());
         }
 
         TEST_CASE("html")
         {
             html h;
             h.description = "description";
-            REQUIRE_EQ("description", h.description());
+            CHECK_EQ("description", h.description());
         }
 
         TEST_CASE("label")
         {
             label l;
             l.description = "description";
-            REQUIRE_EQ("description", l.description());
+            CHECK_EQ("description", l.description());
         }
 
         TEST_CASE("layout")
         {
             layout l;
             l.bottom = "bottom";
-            REQUIRE_EQ("bottom", l.bottom());
+            CHECK_EQ("bottom", l.bottom());
         }
 
         TEST_CASE("number")
         {
             number<double> n;
             n.value = 12.;
-            REQUIRE_EQ(12., n.value());
+            CHECK_EQ(12., n.value());
         }
 
         TEST_CASE("number_bounded")
         {
             number_bounded<double> n;
             n.max = 10.;
-            REQUIRE_EQ(n.max(), 10.);
+            CHECK_EQ(n.max(), 10.);
             n.value = n.max() + 1.;
-            REQUIRE_EQ(n.max(), n.value());
+            CHECK_EQ(n.max(), n.value());
             n.value = n.min() - 1.;
-            REQUIRE_EQ(n.min(), n.value());
-            REQUIRE_THROWS_AS(n.min = n.max() + 1., std::runtime_error);
-            REQUIRE_THROWS_AS(n.max = n.min() - 1., std::runtime_error);
+            CHECK_EQ(n.min(), n.value());
+            CHECK_THROWS_AS(n.min = n.max() + 1., std::runtime_error);
+            CHECK_THROWS_AS(n.max = n.min() - 1., std::runtime_error);
         }
 
         TEST_CASE("password")
         {
             password p;
             p.description = "description";
-            REQUIRE_EQ("description", p.description());
+            CHECK_EQ("description", p.description());
         }
 
         TEST_CASE("play")
         {
             play p;
             p.interval = 15;
-            REQUIRE_EQ(15, p.interval());
+            CHECK_EQ(15, p.interval());
         }
 
         TEST_CASE("progress_style")
         {
             progress_style p;
             p.description_width = "40px";
-            REQUIRE_EQ("40px", p.description_width());
+            CHECK_EQ("40px", p.description_width());
         }
 
         TEST_CASE("progress")
         {
             progress<double> p;
             p.orientation = "vertical";
-            REQUIRE_EQ("vertical", p.orientation());
+            CHECK_EQ("vertical", p.orientation());
         }
 
         TEST_CASE("slider_style")
         {
             slider_style s;
             s.description_width = "40px";
-            REQUIRE_EQ("40px", s.description_width());
+            CHECK_EQ("40px", s.description_width());
         }
 
         TEST_CASE("slider")
         {
             slider<double> s;
             s.value = 2.0;
-            REQUIRE_EQ(2., s.value());
+            CHECK_EQ(2., s.value());
         }
 
         TEST_CASE("text")
         {
             text t;
             t.disabled = true;
-            REQUIRE_EQ(true, t.disabled());
+            CHECK_EQ(true, t.disabled());
         }
 
         TEST_CASE("textarea")
         {
             textarea t;
             t.disabled = true;
-            REQUIRE_EQ(true, t.disabled());
+            CHECK_EQ(true, t.disabled());
         }
 
         TEST_CASE("togglebutton")
         {
             togglebutton t;
             t.tooltip = "tooltip";
-            REQUIRE_EQ("tooltip", t.tooltip());
+            CHECK_EQ("tooltip", t.tooltip());
         }
 
         TEST_CASE("valid")
         {
             valid v;
             v.readout = "readout";
-            REQUIRE_EQ("readout", v.readout());
+            CHECK_EQ("readout", v.readout());
         }
     }
 }
