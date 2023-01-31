65c5c9f472470ef6655c99d4bd764f0f156f35c3
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-01-31 17:22:57 +0100

    Add const iterator on xregistry

--- src/xregistry.cpp
+++ src/xregistry.cpp
@@ -1,3 +1,11 @@
+/***************************************************************************
+ * Copyright (c) 2022, QuantStack and XWidgets contributors                 *
+ *                                                                          *
+ * Distributed under the terms of the BSD 3-Clause License.                 *
+ *                                                                          *
+ * The full license is in the file LICENSE, distributed with this software. *
+ ****************************************************************************/
+
 #include "xwidgets/xregistry.hpp"
 
 namespace xw
@@ -15,6 +23,26 @@ namespace xw
             throw std::runtime_error("Could not find specified id in transport registry");
         }
         return it->second;
+    }
+
+    auto xregistry::begin() const -> const_iterator
+    {
+        return cbegin();
+    }
+
+    auto xregistry::cbegin() const -> const_iterator
+    {
+        return m_storage.cbegin();
+    }
+
+    auto xregistry::end() const -> const_iterator
+    {
+        return cend();
+    }
+
+    auto xregistry::cend() const -> const_iterator
+    {
+        return m_storage.cend();
     }
 
     xregistry& get_transport_registry()
